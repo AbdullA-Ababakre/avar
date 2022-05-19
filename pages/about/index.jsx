712874f9133eab3e04dab0711890237d329cf93e
abdulla <abdullaszu.007@bytedance.com>
2022-05-19 09:38:41 +0800

    feat:fix the module problem

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -8,12 +8,45 @@ import star from '/public/images/homePage/star.png';
 import title1 from '/public/images/about/title1.png';
 import title2 from '/public/images/about/title2.png';
 import title3 from '/public/images/about/title3.png';
-import Footer from '../../components/Footer/index';
+// import Footer from '../../components/Footer/index';
 import NavBar from '../../components/navBar/index';
 
+import Link from 'next/link';
+import facebook from '../../public/images/homePage/share/Facebook.png';
+// import Github from '../../public/images/homePage/share/Github.png';
+import Instagram from '../../public/images/homePage/share/Instagram.png';
+import LinkedIn from '../../public/images/homePage/share/Linkedin.png';
+import Pinterest from '../../public/images/homePage/share/Pinterest.png';
+import Twitter from '../../public/images/homePage/share/Twitter.png';
+
 const About = () => {
+
+  const imageArr = [facebook, LinkedIn, Twitter, Pinterest, Instagram];
+  const linkArr = [
+    {
+      href: '/home',
+      title: 'Home'
+    },
+    {
+      href: '/avatar',
+      title: 'Avatar'
+    },
+    {
+      href: '/fashion',
+      title: 'Digital Fashion'
+    },
+    {
+      href: '/art',
+      title: 'Digital Art Toy'
+    },
+    {
+      href: '/about',
+      title: 'About US'
+    }
+  ];
+
   return (
-    <div className={styles.container}>
+    <div className={styles.footerContainer}>
       <NavBar></NavBar>
       <div className={styles.logoContainer}>
         <Image className={styles.logoLeft} src={logoLeft} alt="logoLeft"></Image>
@@ -57,7 +90,37 @@ const About = () => {
           <Image className={styles.title3} src={title3} alt="title3" />
         </div>
       </div>
-      <Footer />
+      {/* <Footer /> */}
+      <div className={styles.container}>
+        <div className={styles.horizontal}></div>
+        <div className={styles.navBar}>
+          {
+            linkArr.map((item, index) => {
+              return (
+                <Link href={item.href} key={index}>
+                  <a className={styles.item}>{item.title}</a>
+                </Link>
+              )
+            })
+          }
+        </div>
+        <div className={styles.horizontal}></div>
+        <div className={styles.share}>
+          {
+            imageArr.map((item, index) => {
+              return (
+                <div className={styles.item} key={index}>
+                  <Image className={styles.icon} src={item} alt={item}></Image>
+                  {
+                    index !== imageArr.length - 1 && <div className={styles.vertical}></div>
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className={styles.horizontal}></div>
+      </div>
     </div >
   )
 }
--- pages/avatardetail/[id].jsx
+++ pages/avatardetail/[id].jsx
@@ -18,6 +18,13 @@ import ThreeRender from "../../utils/three-render";
 // import wingGLTF from '../../public/models/cubes.gltf';
 import { useRouter } from 'next/router';
 import { avars } from '../../content/index';
+import Link from 'next/link';
+import facebook from '../../public/images/homePage/share/Facebook.png';
+// import Github from '../../public/images/homePage/share/Github.png';
+import Instagram from '../../public/images/homePage/share/Instagram.png';
+import LinkedIn from '../../public/images/homePage/share/Linkedin.png';
+import Pinterest from '../../public/images/homePage/share/Pinterest.png';
+import Twitter from '../../public/images/homePage/share/Twitter.png';
 
 
 const insItem = [{
@@ -102,6 +109,30 @@ const About = () => {
         }
     }, [id]);
 
+    const imageArr = [facebook, LinkedIn, Twitter, Pinterest, Instagram];
+    const linkArr = [
+        {
+            href: '/home',
+            title: 'Home'
+        },
+        {
+            href: '/avatar',
+            title: 'Avatar'
+        },
+        {
+            href: '/fashion',
+            title: 'Digital Fashion'
+        },
+        {
+            href: '/art',
+            title: 'Digital Art Toy'
+        },
+        {
+            href: '/about',
+            title: 'About US'
+        }
+    ];
+
     return (
         <div className="">
             <NavBar></NavBar>
@@ -176,7 +207,37 @@ const About = () => {
                                 cardInfo.map((item, index) => <HomePageCard style={{ width: '25%' }} key={index} item={item} />)
                             }
                         </div>
-                        <Footer></Footer>
+                        {/* <Footer></Footer> */}
+                        <div className={styles.footerContainer}>
+                            <div className={styles.horizontal}></div>
+                            <div className={styles.navBar}>
+                                {
+                                    linkArr.map((item, index) => {
+                                        return (
+                                            <Link href={item.href} key={index}>
+                                                <a className={styles.item}>{item.title}</a>
+                                            </Link>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className={styles.horizontal}></div>
+                            <div className={styles.share}>
+                                {
+                                    imageArr.map((item, index) => {
+                                        return (
+                                            <div className={styles.item} key={index}>
+                                                <Image className={styles.icon} src={item} alt={item}></Image>
+                                                {
+                                                    index !== imageArr.length - 1 && <div className={styles.vertical}></div>
+                                                }
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className={styles.horizontal}></div>
+                        </div>
                     </div>
                 )
             }
